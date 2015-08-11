965e94ecd8ffc0ecb42d4062747dfe73d15a559b
tony <alapenna@workit.fr>
2015-08-11 09:46:00 +0200

    Added basic tests to check: * required files are created * content of package.json is correct

---
+++ test/scaffold-application.js
@@ -0,0 +1,34 @@
+var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-generator').assert;
+var path = require('path');
+var _ = require('lodash');
+
+describe('AngularJS project generation', function () {
+  beforeEach(function (done) {
+    this.answers = {
+      "author": "testAuthor"
+    };
+
+    helpers.run(path.join(__dirname, '../app'))
+      .withArguments(['testProject'])
+      .withPrompts(this.answers)
+      .on('end', done);
+    this.projectName = _.camelCase('testProject');
+    this.author = "testAuthor";
+  });
+
+  it('creates all required files', function () {
+    assert.file([
+      'package.json',
+    ]);
+  });
+
+  it('fills package.json with correct information', function () {
+    var content = [
+      ['package.json', new RegExp('"author": "' + this.author + '"')],
+      ['package.json', new RegExp('"name": "' + this.projectName + '"')]
+    ];
+    assert.fileContent(content);
+  });
+
+});
