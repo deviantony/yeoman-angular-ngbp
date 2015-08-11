1b3d9e53c8eede72eb03f566f272f7f128627884
tony <alapenna@workit.fr>
2015-08-11 09:25:12 +0200

    Added prompt for author. Added generation function with package.json template generation.

--- app/index.js
+++ app/index.js
@@ -9,6 +9,37 @@ module.exports = generators.Base.extend({
     this.argument('projectName', { type: String, required: false });
     this.projectName = this.projectName || path.basename(process.cwd());
     this.projectName = _.camelCase(this.projectName);
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    prompts = [
+      {
+        type    : 'input',
+        name    : 'author',
+        message : 'Project author or company name',
+        store   : true
+      }
+    ]
+
+    this.prompt(prompts, function (answers) {
+      this.author = answers.author;
+      done();
+    }.bind(this));
+  },
+
+  generation: function() {
+    context = {
+      projectName: this.projectName,
+      author: this.author
+    }
+
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      context
+    );
   }
 
 });
